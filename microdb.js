85b2ed885396e9efdd2a5cd7c474d27f97e11e24
Michael B. Kulik <mike.b.kulik@gmail.com>
2015-03-13 14:13:06 -0400

    automatically build a route for each file in the public directory

--- microdb.js
+++ microdb.js
@@ -2,49 +2,33 @@
 var fs = require('fs');
 var express = require('express');
 var jsonql = require('jsonql');
+
 var app = express();
+var router = express.Router();
+var fileDirectory = __dirname + '/public';
 
-//app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res){
-	res.send('Hello World');
+router.get('/', function(req, res){
+	res.send('Hello World')
 });
 
-app.get('/myfile', function(req, res){
-	
+fs.readdirSync(fileDirectory).forEach(function(file){
+	router.get('/' + file, function(req, res){
+		fs.readFile(fileDirectory + '/' + file, function(err, data){
+			if(err) {
+				console.log('Error: ' + err);
+				res.send(err);
+			}
 
+			content = JSON.parse(data)
 
+			answer = jsonql(req.query.query, content)
 
-
-	fs.readFile(__dirname + '/public/myfile', function(err, data){
-		if(err) {
-			console.log('Error: ' + err);
-			res.send(err);
-		}
-
-
-		console.log(req.query.query)
-
-		content = JSON.parse(data)
-
-		answer = jsonql(req.query.query, content)
-
-		console.log();
-
-//res.send(content);
-		res.send(JSON.stringify(answer));
-		
+			res.send(JSON.stringify(answer));
+		});
 	});
-
-
-
-
-
-	//console.log(req.query.query);
-	//res.send(req.query.query);
-
 });
 
+app.use('/', router)
 
 var server = app.listen(8080, function() {
 	var host = server.address().address;
